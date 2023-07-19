d826c28823c58f1bc47e5b9fdb3bc1b8a633bd00
han <maihanbrvt2410@gmail.com>
2023-07-19 12:08:21 +0700

    commit again

--- index.js
+++ index.js
@@ -1,108 +1,73 @@
 
-    const ncolor = '#333333'
-    const nmode = 'color'
-    const size = 16
-    
-    let cColor = ncolor
-    let cMode = nmode
-    let cSize = size
-    
-    function setCurrentColor(newColor) {
-      cColor = newColor
-    }
-    
-    function setCurrentMode(newMode) {
-      activateButton(newMode)
-      cMode = newMode
-    }
-    
-    function setCurrentSize(newSize) {
-      cSize = newSize
-    }
-    const btn = document.getElementById('btn')
-    const btn1 = document.getElementById('btn1')
-    const btn2 = document.getElementById('btn2')
-    const btn3 = document.getElementById('btn3')
-    const btn4 = document.getElementById('btn4')
-    const sizeValue = document.getElementById('sizeValue')
-    const sizeSlider = document.getElementById('sizeSlider')
-    const container = document.getElementById('container')
+let click = false;
 
-btn.oneclick = (e) => setCurrentColor(e.target.value)
-btn1.onclick = () => setCurrentMode('color')
-btn2.onclick = () => setCurrentMode('rainbow')
-btn3.onclick = () => setCurrentMode('eraser')
-btn4.onclick = () => reloadGrid()
-sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
-sizeSlider.onchange = (e) => changeSize(e.target.value)
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
 
-  let mouseDown = false
-    document.body.onmousedown = () => (mouseDown = true)
-    document.body.onmouseup = () => (mouseDown = false)
-
-    function changeSize(value) {
-        setCurrentSize(value)
-        updateSizeValue(value)
-        reloadGrid()
-      }
-      function updateSizeValue(value) {
-        sizeValue.innerHTML = `${value} x ${value}`
-      }
-      function reloadGrid() {
-        clearGrid()
-        createBoard(cSize)}
-
-    function clearGrid() {
-            container.innerHTML = ''
-          }
-    function createBoard(size){
-            container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-            container.style.gridTemplateRows = `repeat(${size}, 1fr)`; 
-        
-            let numDivs = size * size;
-        
-            for(let i = 0; i < numDivs; i++){
-                let div = document.createElement("div");
-                div.classList.add('grid-element')
-                div.addEventListener("mouseover", changeColor)
-                div.addEventListener('mousedown', changeColor)
-                container.appendChild( div);
-            }      
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML = " Draw";
+            }
+            else{
+                draw.innerHTML = "Allowed To Draw"
+            }
         }
+    })
 
-    
-        function changeColor(e) {
-            if (e.type === 'mouseover' && !mouseDown) return
-            if (cMode === 'btn2') {
-              const randomR = Math.floor(Math.random() * 256)
-              const randomG = Math.floor(Math.random() * 256)
-              const randomB = Math.floor(Math.random() * 256)
-             console.log(`rgb(${randomR}, ${randomG}, ${randomB})`)
-            } else if (cMode === 'btn1') {
-              console.log(e)= cColor
-            } else if (cMode === 'btn3') {
-              console.log(e) = '#fefefe'
-            }
-          }
-          function activateButton(newMode) {
-            if (cMode === 'btn2') {
-              btn2.classList.remove('active')
-            } else if (cMode === 'color') {
-              btn1.classList.remove('active')
-            } else if (cMode === 'eraser') {
-             btn3.classList.remove('active')
-            }
-          
-            if (newMode === 'btn2') {
-              btn2.classList.add('active')
-            } else if (newMode === 'btn1') {
-              btn1.classList.add('active')
-            } else if (newMode === 'btn3') {
-              btn3.classList.add('active')
-            }
-          }
-          
-          window.onload = () => {
-            createBoard(size)
-            activateButton(nmode)
-          }
+    let select = document.querySelector("#select");
+    select.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
+})
+function getSize(){
+    let input = prompt("Choose the size of the board?");
+    let mess = document.querySelector("#mess");
+    if(input == ""){
+        mess.innerHTML = "choose a number";
+    }
+    else if(input < 0 || input > 100){
+        mess.innerHTML = "between 1 and 100"
+    }
+    else{
+        mess.innerHTML = "GO!"
+        return input;
+    }
+}
+function createBoard(size){
+    let container = document.querySelector(".container");
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`; 
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", colorDiv)
+        container.insertAdjacentElement("beforeend", div);
+    }
+
+}
+function colorDiv(){
+    if(click){
+        if(color == "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else{
+            this.style.backgroundColor = 'purple'
+        }
+    }
+}
+let color = "purple";
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
+}
